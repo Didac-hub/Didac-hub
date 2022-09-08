3a3c3c1b7f2f1dc9801c8778fcd80c9751c85b48
Didac Balboa <58149378+Didac-hub@users.noreply.github.com>
2022-09-08 21:00:06 +0200

    Create Act2.php

---
+++ Act2.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Titulo de primer proyecto </title>
+</head>
+<body>
+
+<?php
+echo "Texto básico en PHP para el proyecto...";
+?>
+
+</body>
+</html>
